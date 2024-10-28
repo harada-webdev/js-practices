57eb96e75bc02b3f78182ae6ccaa739b5f2a103e
harada-webdev <hrdk74531@outlook.jp>
2024-10-28 14:51:22 +0900

    Add cal

---
+++ 02.calendar/cal.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+const argv = minimist(process.argv.slice(2), {
+  default: {
+    y: new Date().getFullYear(),
+    m: new Date().getMonth() + 1,
+  },
+});
+const year = argv.y;
+const month = argv.m;
+const firstDay = new Date(year, month - 1);
+const lastDay = new Date(year, month, 0);
+
+console.log(`      ${month}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
+process.stdout.write("   ".repeat(firstDay.getDay()));
+for (let day = firstDay; day <= lastDay; day.setDate(day.getDate() + 1)) {
+  const formattedDay = day.getDate().toString().padStart(2);
+  if (day.getDay() === 6 && day < lastDay) {
+    process.stdout.write(`${formattedDay}\n`);
+  } else {
+    process.stdout.write(`${formattedDay} `);
+  }
+}
+console.log(`\n`);
