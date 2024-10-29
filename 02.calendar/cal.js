854a5d30fc65b1d5a445fa3f8df5525a0af5f211
harada-webdev <hrdk74531@outlook.jp>
2024-10-29 11:37:28 +0900

    Replace some padStart() with repeat()

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -12,9 +12,9 @@ const argv = minimist(process.argv.slice(2), {
 const firstDay = new Date(argv.y, argv.m - 1);
 const lastDay = new Date(argv.y, argv.m, 0);
 
-console.log(`${"".padStart(6)}${argv.m}月 ${argv.y}`);
+console.log(`${" ".repeat(6)}${argv.m}月 ${argv.y}`);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("".padStart(firstDay.getDay() * 3));
+process.stdout.write(" ".repeat(firstDay.getDay() * 3));
 for (let day = firstDay; day <= lastDay; day.setDate(day.getDate() + 1)) {
   process.stdout.write(`${day.getDate().toString().padStart(2).padEnd(3)}`);
   process.stdout.write(day.getDay() === 6 && day < lastDay ? `\n` : "");
