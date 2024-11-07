ed590683c460f9e91bbc7d9731a11cdf170d8fc0
harada-webdev <hrdk74531@outlook.jp>
2024-11-07 20:34:32 +0900

    Add async-await

---
+++ 03.asynchronous/async-await.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import { db, runQueryPromise, getQueryPromise } from "./promise-functions.js";
+
+(async () => {
+  await runQueryPromise(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+
+  const result = await runQueryPromise(
+    db,
+    "INSERT INTO books (title) VALUES (?)",
+    "JavaScriptの本",
+  );
+  console.log(`id: ${result.lastID}`);
+
+  const record = await getQueryPromise(
+    db,
+    "SELECT id, title FROM books WHERE id = ?",
+    result.lastID,
+  );
+  console.log(record);
+
+  await runQueryPromise(db, "DROP TABLE books");
+
+  await runQueryPromise(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+
+  try {
+    await runQueryPromise(db, "INSERT INTO books (title) VALUES (?)");
+  } catch (err) {
+    console.error(err.message);
+  }
+
+  try {
+    await getQueryPromise(db, "SELECT body FROM books WHERE id = ?", 1);
+  } catch (err) {
+    console.error(err.message);
+  }
+
+  runQueryPromise(db, "DROP TABLE books");
+})();
