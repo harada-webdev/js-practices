fadacf9c9218a7f53abe8e6db3b4e2b72384616b
harada-webdev <hrdk74531@outlook.jp>
2024-11-08 19:11:07 +0900

    Use Top-level await

--- 03.asynchronous/async-await.js
+++ 03.asynchronous/async-await.js
@@ -2,44 +2,42 @@
 
 import { db, runQueryPromise, getQueryPromise } from "./promise-functions.js";
 
-(async () => {
-  await runQueryPromise(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runQueryPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  const result = await runQueryPromise(
-    db,
-    "INSERT INTO books (title) VALUES (?)",
-    "JavaScriptの本",
-  );
-  console.log(`id: ${result.lastID}`);
+const result = await runQueryPromise(
+  db,
+  "INSERT INTO books (title) VALUES (?)",
+  "JavaScriptの本",
+);
+console.log(`id: ${result.lastID}`);
 
-  const record = await getQueryPromise(
-    db,
-    "SELECT id, title FROM books WHERE id = ?",
-    result.lastID,
-  );
-  console.log(record);
+const record = await getQueryPromise(
+  db,
+  "SELECT id, title FROM books WHERE id = ?",
+  result.lastID,
+);
+console.log(record);
 
-  await runQueryPromise(db, "DROP TABLE books");
+await runQueryPromise(db, "DROP TABLE books");
 
-  await runQueryPromise(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runQueryPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  try {
-    await runQueryPromise(db, "INSERT INTO books (title) VALUES (?)");
-  } catch (err) {
-    console.error(err.message);
-  }
+try {
+  await runQueryPromise(db, "INSERT INTO books (title) VALUES (?)");
+} catch (err) {
+  console.error(err.message);
+}
 
-  try {
-    await getQueryPromise(db, "SELECT body FROM books WHERE id = ?", 1);
-  } catch (err) {
-    console.error(err.message);
-  }
+try {
+  await getQueryPromise(db, "SELECT body FROM books WHERE id = ?", 1);
+} catch (err) {
+  console.error(err.message);
+}
 
-  await runQueryPromise(db, "DROP TABLE books");
-})();
+await runQueryPromise(db, "DROP TABLE books");
