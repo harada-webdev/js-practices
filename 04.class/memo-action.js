a43cf55de69000cc7126875ce92ea60ff6efc52d
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2024-12-04 23:13:26 +0900

    Rename getMessage to getInstructionMessage

--- 04.class/memo-action.js
+++ 04.class/memo-action.js
@@ -120,7 +120,7 @@ export default class MemoAction {
     return {
       type: "select",
       name: purpose,
-      message: this.#getMessage(purpose),
+      message: this.#getInstructionMessage(purpose),
       choices: memos.map((memo) => ({
         message: this.#getMemoTitle(memo),
         name: this.#getMemoTitle(memo),
@@ -135,7 +135,7 @@ export default class MemoAction {
     };
   }
 
-  #getMessage(purpose) {
+  #getInstructionMessage(purpose) {
     if (purpose === "show") {
       return "表示するメモを選んでください";
     } else if (purpose === "delete") {
